e74ff9bcf93558b7f355b7db28d2448adce11297
DevGadBadr <gad.elhak23@gmail.com>
2026-07-31 20:59:52 +0300

    resize enhance

--- elementSetup.py
+++ elementSetup.py
@@ -5,25 +5,20 @@ from PyQt5 import QtWidgets
 def setUpSplitters(robustClass):
     hSplitter: QtWidgets.QSplitter = robustClass.mainHorizontalSplitter
     vSplitter: QtWidgets.QSplitter = robustClass.controlVerticalSplitter
-    minWidth = 100
-    minHeight = 100
+    minWidth = 500
+    minHeight = 200
 
     robustClass.driverWindow.setMinimumWidth(minWidth)
     robustClass.controlPanel.setMinimumWidth(minWidth)
     robustClass.statusArea.setMinimumHeight(minHeight)
     robustClass.scrollArea.setMinimumHeight(minHeight)
 
-    for splitter in (hSplitter, vSplitter):
-        splitter.setChildrenCollapsible(False)
-
-    # driverWindow absorbs window resize; controlPanel keeps its width unless user drags
+    # DriverWindow absorbs window resize; controlPanel keeps its width unless user drags
     hSplitter.setStretchFactor(0, 1)
     hSplitter.setStretchFactor(1, 0)
     vSplitter.setStretchFactor(0, 1)
     vSplitter.setStretchFactor(1, 1)
 
-    # controlVerticalSplitter is item index 4 in verticalLayout
-    # (after chooseLabel, head, defaultJob, createButton)
     robustClass.verticalLayout.setStretch(4, 1)
 
     def applyHorizontalSizes():
